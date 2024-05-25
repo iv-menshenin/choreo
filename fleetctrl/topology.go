c344c600969c74e27361311e968f4416ba12b785
devalio <github@devalio.ru>
2024-05-25 21:15:11 +0300

    added Fleet() method that allows to discover IP addresses

---
+++ fleetctrl/topology.go
@@ -0,0 +1,14 @@
+package fleetctrl
+
+import "net"
+
+func (m *Manager) Fleet() []net.Addr {
+	var addrs = make([]net.Addr, 0, m.keeper.Len())
+	for _, id := range m.keeper.IDs() {
+		shard, ok := m.keeper.Shard(id)
+		if ok {
+			addrs = append(addrs, shard.Addr)
+		}
+	}
+	return addrs
+}
