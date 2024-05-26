ed7a5a33c981affa0ba88ab0d342ecb441eb5c73
devalio <github@devalio.ru>
2024-05-26 19:25:51 +0300

    return only alive shards

--- fleetctrl/topology.go
+++ fleetctrl/topology.go
@@ -6,7 +6,7 @@ func (m *Manager) Fleet() []net.Addr {
 	var addrs = make([]net.Addr, 0, m.keeper.Len())
 	for _, id := range m.keeper.IDs() {
 		shard, ok := m.keeper.Shard(id)
-		if ok {
+		if ok && shard.Live() {
 			addrs = append(addrs, shard.Addr)
 		}
 	}
